cac9ece7872883c109ce29e29307a5a4acb97164
abhinjoesaji-cmyk <abhinjoesaji@gmail.com>
2026-01-26 17:37:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-let buttons = document.querySelectorAll("button");
+function addToCart(name, price, image) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        alert("Product added to cart!");
-    });
-});
+    // Check if item already exists
+    let existingItem = cart.find(item => item.name === name);
+    if (existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        cart.push({
+            name: name,
+            price: price,
+            image: image,
+            quantity: 1
+        });
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(name + " added to cart!");
+}
+
+function removeFromCart(index) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    location.reload(); // Refresh to show changes
+}
+
+function clearCart() {
+    localStorage.removeItem("cart");
+    location.reload();
+}
